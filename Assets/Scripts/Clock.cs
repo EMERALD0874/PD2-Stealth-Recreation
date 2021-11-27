b51d078c5e7e1cd1b5f9ff91723f170d453ad061
Hudson <hudson.samuels@gmail.com>
2021-11-27 00:34:25 -0600

    Added UI and pager voice lines

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    TextMeshProUGUI _textObject;
+
+    private void Start()
+    {
+        _textObject = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.timeSinceLevelLoad;
+
+        string m = Mathf.FloorToInt(t / 60).ToString().PadLeft(2, '0');
+        string s = Mathf.FloorToInt(t % 60).ToString().PadLeft(2, '0');
+
+        _textObject.text = m + ":" + s;
+    }
+}
