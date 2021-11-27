dc99cb0a01573df3b7d4e10c87befd8c0efe0a56
Hudson <hudson.samuels@gmail.com>
2021-11-26 22:59:08 -0600

    Added character/mouse movement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float _mouseSens = 100f;
+    [SerializeField] Transform _playerTransform;
+
+    float _xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSens * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -89f, 89f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float _speed = 12f;
+    [SerializeField] float _sprint = 1.5f;
+    [SerializeField] float _gravity = -9.81f;
+    [SerializeField] float _jumpHeight = 3f;
+    public bool _canMove = true;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundDistance = .4f;
+    [SerializeField] LayerMask _groundMask;
+
+    [Header("Visuals")]
+    [SerializeField] float _normalFov = 90f;
+
+    CharacterController _controller;
+    Vector3 _velocity;
+    bool _isGrounded;
+    Camera _mainCam;
+    AudioSource _source;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _mainCam = Camera.main;
+        _source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if grounded
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+        if(_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -1f;
+        }
+
+        // Move on X and Z axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            move *= _sprint;
+        if (!_canMove)
+            move *= 0;
+        
+        _controller.Move(move * _speed * Time.deltaTime);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && _isGrounded && _canMove)
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+
+        // Gravity
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+}
