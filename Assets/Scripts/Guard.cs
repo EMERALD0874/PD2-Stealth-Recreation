5f00def9c19ccd40d2573febbdfef915b9cfb23f
Hudson <hudson.samuels@gmail.com>
2021-11-28 23:02:16 -0600

    Added patrolling to guards

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -4,22 +4,96 @@ using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] Animator animator;
+    [SerializeField] float walkSpeed = 3f;
+    [SerializeField] float turnTime = .5f;
+    [SerializeField] float waitMin = 3f;
+    [SerializeField] float waitMax = 10f;
+    [SerializeField] float waitChance = .5f;
+    [Header("Waypoints")]
+    [SerializeField] Transform waypointParent;
+    [SerializeField] Waypoint startingWaypoint;
 
     bool alert;
 
     private void Start()
     {
         alert = false;
-    }
-
-    private void Update()
-    {
         animator.SetBool("IsAlerted", alert);
+
+        StartCoroutine(Patrol(startingWaypoint));
     }
 
     public void Alert()
     {
         alert = true;
+        animator.SetBool("IsAlerted", alert);
+    }
+
+    IEnumerator Patrol(Waypoint w)
+    {
+        Waypoint current = w;
+        int i = Random.Range(0, current.connectedWaypoints.Length);
+        Waypoint target = current.connectedWaypoints[i];
+        transform.LookAt(target.transform);
+
+        animator.SetBool("IsWalking", true);
+
+        while (!alert)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, walkSpeed * Time.deltaTime);
+
+            if (transform.position == target.transform.position)
+            {
+                // Generate a new waypoint
+                current = target;
+                i = Random.Range(0, current.connectedWaypoints.Length);
+                target = current.connectedWaypoints[i];
+
+                // Decide if we're waiting if we can stop AND chance passes
+                // if (current.canStop)
+                if (current.canStop && Random.Range(0f, 1f) >= waitChance)
+                {
+                    animator.SetBool("IsWalking", false);
+
+                    // Rotate to where the current waypoint wants us to look
+                    yield return StartCoroutine(Rotate(current.transform.rotation));
+
+                    // Wait a bit
+                    float waitTime = Random.Range(waitMin, waitMax);
+                    yield return new WaitForSeconds(waitTime);
+                }
+                
+                if (!alert)
+                    yield return StartCoroutine(Rotate(Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up)));
+                animator.SetBool("IsWalking", true);
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator Rotate(Quaternion q)
+    {
+        float completion = 0f;
+        float startTime = Time.time;
+        float turnSpeed = 1 / turnTime;
+
+        Quaternion original = transform.rotation;
+
+        while (completion < 1f)
+        {
+            completion = (Time.time - startTime) * turnSpeed;
+            transform.rotation = Quaternion.Lerp(original, q, completion);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        foreach (Transform waypoint in waypointParent)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+        }
     }
 }
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public bool canStop = true;
+    public Waypoint[] connectedWaypoints;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, transform.forward * 2.5f);
+        Gizmos.DrawSphere(transform.position, .3f);
+
+        foreach (Waypoint w in connectedWaypoints)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(w.transform.position, .3f);
+            Gizmos.DrawLine(transform.position, w.transform.position);
+        }
+    }
+}
